db3c670f51bc94c5a401ff3d71819ea2399ae966
Eduardo Salinas <eduardo.salinas1@outlook.com>
2021-07-05 17:57:27 -0300

    Home and Loading
    se instalo react-router y react spinners

---
+++ client/src/components/Home/index.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Loading from '../Loading';
+import { getProductsAll } from '../../redux/actions'
+
+export const Home = () => {
+    const dispatch = useDispatch();
+    const store = useSelector(state => state);
+
+    useEffect(() => {
+        dispatch(getProductsAll());
+    }, [dispatch]);
+
+    return (
+        <>
+            {store.loading ? <Loading /> :
+                (<div>
+                    {store.products.length > 0 ?
+                        store.products.map(product =>
+                            <>aca se renderiza las card de products con las props</>) :
+                        <div>
+                            <h1>Products not found</h1>
+                            <button type="button" onClick={() => { dispatch(getProductsAll()) }}>Return</button>
+                        </div>}
+                </div>
+                )}
+        </>
+    )
+};
+
+export default Home;
