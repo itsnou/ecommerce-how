7dca4b8ff71bcdd6e7411d4cfb34688592e78694
Juan Sebastian Salazar <juansebastiansalazargonzalez@gmail.com>
2021-07-06 09:41:25 -0500

    Post products route
    Se creó la ruta post para los productos, se testea y postea bien en la base de datos

--- api/src/models/products.js
+++ api/src/models/products.js
@@ -1,56 +1,56 @@
 const mongoose = require("mongoose");
 
 const ProductsSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        price: {
-            type: Number,
-            required: false,
-        },
-        vineyard: {
-            type: String,
-            required: false,
-        },
-        rating: [Number],
-        description: {
-            type: String,
-            required: false,
-        },
-        category: {
-            type: String,
-            required: false,
-            enum: ["red", "white", "rose"], //AGREGAR LUEGO MAS CATEGORIAS SI ES NECESARIO
-        },
-        stock: {
-            type: Number,
-            required: false,
-        },
-        discount: {
-            type: Number,
-            required: false,
-            default: 0,
-        },
-        imageUrl: {
-            type: String,
-            requiered: true,
-        },
-        varietal: [String],
-        year: {
-            type: Number,
-            required: false,
-        },
-        quantity: {
-            type: Number,
-            require: true,
-            default: 1,
-        },
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        versionKey: false,
-    }
+    price: {
+      type: Number,
+      required: true,
+    },
+    vineyard: {
+      type: String,
+      required: true,
+    },
+    rating: [Number],
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ["red", "white", "rose"], //AGREGAR LUEGO MAS CATEGORIAS SI ES NECESARIO
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    imageUrl: {
+      type: String,
+      requiered: true,
+    },
+    varietal: [String],
+    year: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      require: true,
+      default: 1,
+    },
+  },
+  {
+    versionKey: false,
+  }
 );
 
 const Product = mongoose.model("Product", ProductsSchema, "products");
