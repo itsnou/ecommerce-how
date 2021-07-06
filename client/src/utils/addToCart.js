114480523375e91bc91e3a643ab5c650fe1513b7
manuelnsosa <manuelsosa84@gmail.com>
2021-07-06 18:54:17 -0300

    Modified actions, reducers, added new component Cart and created new folder Utils

---
+++ client/src/utils/addToCart.js
@@ -0,0 +1,13 @@
+export const addToCart = (obj, array) => {
+  const match = array.find((e) => obj._id === e._id);
+  if (match === undefined) {
+    return array.concat(obj);
+  } else {
+    return array.map((e) => {
+      if (e._id === obj._id) {
+        e = { ...e, quantity: obj.quantity };
+      }
+      return e;
+    });
+  }
+};
