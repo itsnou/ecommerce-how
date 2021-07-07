c202febeeaf40d8ae02483ca3ccdea8d2fb4255a
Juan Sebastian Salazar <juansebastiansalazargonzalez@gmail.com>
2021-07-07 16:50:30 -0500

    fix search state
    Co-Authored-By: Eduardo Salinas <75259006+eduardo-salinas@users.noreply.github.com>
    Co-Authored-By: Juan Condori <78325127+juancdri@users.noreply.github.com>
    Co-Authored-By: Mr .reverse <75991527+julianmondaca@users.noreply.github.com>
    Co-Authored-By: Manuel Sosa <71500229+manuelnsosa@users.noreply.github.com>
    Co-Authored-By: marianavinyolas <49161320+marianavinyolas@users.noreply.github.com>
    Co-Authored-By: Tomas Buzzalino <tomibuzzalino@gmail.com>
    Co-Authored-By: Emiliano Meneses <65609474+itsnou@users.noreply.github.com>

--- client/src/components/Catalogo/Catalogo.jsx
+++ client/src/components/Catalogo/Catalogo.jsx
@@ -7,65 +7,64 @@ import ReactPaginate from "react-paginate";
 import Loading from "../Loading/Loading";
 
 const Catalogo = () => {
-    const dispatch = useDispatch();
-    const store = useSelector((state) => state);
-    const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const store = useSelector((state) => state);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        dispatch(getProductsAll());
-        dispatch(getProductsByName(""));
-    },[dispatch]);
+  useEffect(() => {
+    dispatch(getProductsAll());
+    dispatch(getProductsByName(""));
+  }, [dispatch]);
 
-    useEffect(() => {
-        if (store.search !== 0) {
-            setProducts(store.search);
-        } else {
-            setProducts(store.products);
-        }
-    }, [store.search]);
+  useEffect(() => {
+    if (store.search !== 0) {
+      setProducts(store.search);
+    } else {
+      setProducts(store.products);
+    }
+  }, [store.search, store.products]);
 
-    //paginated
-    const [pageNumber, setPageNumber] = useState(0);
-    const productsPerPage = 6; // productos a mostrar
-    const pagesVisited = pageNumber * productsPerPage;
-    console.log(products);
-    const displayProducts = products
-        .slice(pagesVisited, pagesVisited + productsPerPage)
-        .map((product, idx) => {
-            return <ProductCard key={idx} product={product} />;
-        });
+  //paginated
+  const [pageNumber, setPageNumber] = useState(0);
+  const productsPerPage = 6; // productos a mostrar
+  const pagesVisited = pageNumber * productsPerPage;
+  const displayProducts = products
+    .slice(pagesVisited, pagesVisited + productsPerPage)
+    .map((product, idx) => {
+      return <ProductCard key={idx} product={product} />;
+    });
 
-    const pageCount = Math.ceil(products.length / productsPerPage);
+  const pageCount = Math.ceil(products.length / productsPerPage);
 
-    const changePage = ({ selected }) => {
-        setPageNumber(selected);
-    };
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
 
-    return (
-        <>
-            {store.loading ? (
-                <Loading />
-            ) : (
-                <StyledDiv>
-                    <div className="filter"></div>
-                    <div className="cards-container">{displayProducts}</div>
-                    <div className="paginate">
-                        <ReactPaginate
-                            previousLabel={"Anterior"}
-                            nextLabel={"Siguiente"}
-                            pageCount={pageCount}
-                            onPageChange={changePage}
-                            containerClassName={"paginationBtn"}
-                            previousLinkClassName={"previousBtn"}
-                            nextLinkClassName={"nextBtn"}
-                            disabledClassName={"paginationDisable"}
-                            activeClassName={"paginationActive"}
-                        />
-                    </div>
-                </StyledDiv>
-            )}
-        </>
-    );
+  return (
+    <>
+      {store.loading ? (
+        <Loading />
+      ) : (
+        <StyledDiv>
+          <div className="filter"></div>
+          <div className="cards-container">{displayProducts}</div>
+          <div className="paginate">
+            <ReactPaginate
+              previousLabel={"Anterior"}
+              nextLabel={"Siguiente"}
+              pageCount={pageCount}
+              onPageChange={changePage}
+              containerClassName={"paginationBtn"}
+              previousLinkClassName={"previousBtn"}
+              nextLinkClassName={"nextBtn"}
+              disabledClassName={"paginationDisable"}
+              activeClassName={"paginationActive"}
+            />
+          </div>
+        </StyledDiv>
+      )}
+    </>
+  );
 };
 
 export default Catalogo;
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -5,33 +5,31 @@ import ProductCard from "../ProductCard/ProductCard";
 import { reset } from "../../redux/actions";
 
 const Home = () => {
-    const dispatch = useDispatch();
-    const store = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const store = useSelector((state) => state);
 
-    useEffect(() => {
-        dispatch(reset("search"));
-        return dispatch(reset("search"));
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(reset("search"));
+    return () => dispatch(reset("search"));
+  }, [dispatch]);
 
-    return (
-        <>
-            {store.loading ? (
-                <Loading />
-            ) : (
-                <div>
-                    {store.search.length > 0 ? (
-                        store.search.map((product) => (
-                            <ProductCard product={product} />
-                        ))
-                    ) : (
-                        <div>
-                            <h1>ACA VA EL BANNER Y LOS VINOS EN OFERTA</h1>
-                        </div>
-                    )}
-                </div>
-            )}
-        </>
-    );
+  return (
+    <>
+      {store.loading ? (
+        <Loading />
+      ) : (
+        <div>
+          {store.search.length > 0 ? (
+            store.search.map((product) => <ProductCard product={product} />)
+          ) : (
+            <div>
+              <h1>ACA VA EL BANNER Y LOS VINOS EN OFERTA</h1>
+            </div>
+          )}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Home;
