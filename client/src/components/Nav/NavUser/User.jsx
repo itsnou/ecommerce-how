46f946e987b4dcf8aa10b346dc20e4eb7f1709d8
Emiliano Meneses <65609474+itsnou@users.noreply.github.com>
2021-07-08 15:36:21 -0300

    Add cart from localStorage
    Ahora el carrito está al tanto de la store de los productos si se reinicia la pagina.
    
    Co-Authored-By: Mr .reverse <75991527+julianmondaca@users.noreply.github.com>
    Co-Authored-By: Manuel Sosa <71500229+manuelnsosa@users.noreply.github.com>

--- client/src/components/Nav/NavUser/User.jsx
+++ client/src/components/Nav/NavUser/User.jsx
@@ -1,22 +1,45 @@
 import {useState, useEffect, useRef} from 'react';
 import {FaShoppingCart} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import { addToCart,modifyItemCart } from "../../../redux/actions/cart";
+
 
 const User = () => {
+    const dispatch=useDispatch();
     const [price, setPrice] = useState(0);
     const countCart = useSelector(state=> state.cart);
-    let counter =0;
-    let fixed = useRef(counter);
+    const [cartCount, setCartCount] = useState(0) 
+    let prces = 0;
+    let counter = 0;
+    let fixed = useRef(prces);
+    let quantityProduct = useRef(counter);
+
+    useEffect(()=>{
+        if(window.localStorage){
+            Object.keys(window.localStorage).map(el=>{
+                dispatch(addToCart(JSON.parse(window.localStorage.getItem(`${el}`))));
+            });
+        }
+    },[dispatch]);
+
     useEffect(() => {
         if(countCart.length > 0){
+            //contador de precios
             let count= countCart.map(e => e.price*e.quantity);
             const reducer = (accumulator, currentValue) => accumulator + currentValue;
             fixed.current = count.reduce(reducer);
             setPrice(fixed.current)
+
+            //contador de cantidades
+            let quantity = countCart.map(e=>e.quantity);
+            quantityProduct.current=quantity.reduce(reducer);
+            setCartCount(quantityProduct.current);
         }
-    }, [countCart]);
-    
+    }, [countCart, dispatch]);
+
+
+
 
     return (  
         <div className='nav-user'>
@@ -24,7 +47,7 @@ const User = () => {
                 <li><Link to='/create'>CREAR CUENTA</Link></li>
                 <li><Link to='/login'>INICIAR SESIÓN</Link></li>
                 <li><Link to='/carrito'><FaShoppingCart/> CARRITO</Link></li>
-                <li className='nav-list_count'>{countCart.length}</li>
+                <li className='nav-list_count'>{cartCount}</li>
                 <li className='nav-list_price'>$ {price}</li>
             </ul>
         </div>
