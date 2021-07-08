9c0eee2d119c488d24923e4786fefac39db9f019
Juan Sebastian Salazar <juansebastiansalazargonzalez@gmail.com>
2021-07-07 19:59:37 -0500

    fix bugs
    Co-Authored-By: Emiliano Meneses <65609474+itsnou@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,22 @@ import { Route } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Catalogo from "./components/Catalogo/Catalogo";
 import ProductDetail from "./components/ProductDetail/ProductDetail";
+import Cart from "./components/Cart/Cart";
 
 import Home from "./components/Home/Home";
 
 function App() {
-    return (
-        <>
-            <Route path={["/", "/vino/:id", "/catalogo"]}>
-                <Nav />
-            </Route>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/catalogo" component={Catalogo} />
-            <Route exact path="/product/:id" component={ProductDetail} />
-        </>
-    );
+  return (
+    <>
+      <Route path={["/", "/vino/:id", "/catalogo"]}>
+        <Nav />
+      </Route>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/catalogo" component={Catalogo} />
+      <Route exact path="/product/:id" component={ProductDetail} />
+      <Route exact path="/carrito" component={Cart} />
+    </>
+  );
 }
 
 export default App;
