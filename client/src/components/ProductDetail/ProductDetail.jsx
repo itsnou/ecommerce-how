c9ce921b1f07b64047e5d4edea03651028a6c5c4
Mr .reverse <75991527+julianmondaca@users.noreply.github.com>
2021-07-09 12:46:43 -0300

    component almost finished

--- client/src/components/ProductDetail/ProductDetail.jsx
+++ client/src/components/ProductDetail/ProductDetail.jsx
@@ -3,17 +3,36 @@ import {useDispatch, useSelector} from 'react-redux';
 import {getProductDetail} from '../../redux/actions/request';
 import {addToCart} from '../../redux/actions/cart';
 import StyledDiv from './style';
+import StarRatingComponent from 'react-star-rating-component';
+import {FaWineGlass} from 'react-icons/fa';
 
 const ProductDetail = ({match}) => {
 	const dispatch = useDispatch();
 	const detail = useSelector((store) => store.productDetail);
+	const cart = useSelector((store) => store.cart);
+	const [count, setCount] = useState(0);
+	const [stars, setStars] = useState(0);
 
 	useEffect(() => {
 		let id = match.params.id;
 		dispatch(getProductDetail(id));
 	}, [dispatch]);
 
-	const [count, setCount] = useState(1);
+	useEffect(() => {
+		if (cart.length > 0) {
+			let wine = cart.find((e) => e._id === detail._id);
+			if (wine) {
+				setCount(wine.quantity);
+			}
+		}
+	}, []);
+
+	useEffect(() => {
+		if (detail.rating) {
+			let totalStars = detail.rating.reduce((a, b) => a + b, 0);
+			setStars(totalStars / detail.rating.length);
+		}
+	}, [detail]);
 
 	const handleClick = () => {
 		let obj = {
@@ -40,17 +59,40 @@ const ProductDetail = ({match}) => {
 							return <h3 key={e}>{e}</h3>;
 						})}
 				</div>
-				<h3>Detalle: {detail.description}</h3>
-				<div className='detail-btn'>
-					<input
-						type='number'
-						min={detail.quantity}
-						max={detail.stock}
-						value={count}
-						onChange={(e) => setCount(e.target.value)}
-					/>
-					<button onClick={() => handleClick()}>AGREGAR</button>
+				<div className='detail-stars'>
+					<h3>
+						puntaje:
+						<StarRatingComponent
+							name='rateWine'
+							editing={false}
+							renderStarIcon={() => (
+								<span>
+									<FaWineGlass />
+								</span>
+							)}
+							starCount={5}
+							value={stars}
+						/>
+					</h3>
 				</div>
+
+				<h3>Detalle: {detail.description}</h3>
+				{detail.stock < 1 ? (
+					<div className='detail-btn'>
+						<h3>SIN STOCK</h3>
+					</div>
+				) : (
+					<div className='detail-btn'>
+						<input
+							type='number'
+							min={detail.quantity}
+							max={detail.stock}
+							value={count}
+							onChange={(e) => setCount(e.target.value)}
+						/>
+						<button onClick={() => handleClick()}>AGREGAR</button>
+					</div>
+				)}
 			</div>
 		</StyledDiv>
 	);
