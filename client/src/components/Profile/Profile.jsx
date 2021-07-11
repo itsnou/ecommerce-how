a321e5966299297e1719a2809e2607d2b75f7c88
Juan Sebastian Salazar <juansebastiansalazargonzalez@gmail.com>
2021-07-11 13:12:30 -0500

    profile rendering user props
    Co-Authored-By: Tomas Buzzalino <tomibuzzalino@gmail.com>
    Co-Authored-By: marianavinyolas <49161320+marianavinyolas@users.noreply.github.com>

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -6,11 +6,29 @@ import { getProfile } from "../../redux/actions/request";
 const Profile = () => {
   const dispatch = useDispatch();
   const loged = useSelector((state) => state.loged);
+  const user = useSelector((state) => state.user);
   useEffect(() => {
     dispatch(getProfile());
   }, [dispatch]);
 
-  return <div>{loged === "on" ? <h1>Logueado</h1> : <h1>No logueado</h1>}</div>;
+  return (
+    <div>
+      {loged === "on" ? (
+        user.map((el) => {
+          return (
+            <div>
+              <h1>{el.name}</h1>
+              <h1>{el.lastName}</h1>
+              <h1>{el.address}</h1>
+              <h1>{el.email}</h1>
+            </div>
+          );
+        })
+      ) : (
+        <h1>No logueado</h1>
+      )}
+    </div>
+  );
 };
 
 export default Profile;
