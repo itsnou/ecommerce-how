4659a6306c104a9cefd26772646d2f17b07dbf37
Juan Sebastian Salazar <juansebastiansalazargonzalez@gmail.com>
2021-07-12 09:44:51 -0500

    fix request action LOAD_PROFILE

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,39 +1,52 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { logIn } from "../../redux/actions/sending";
 import { getProfile } from "../../redux/actions/request";
-import StyledDiv from './styled';
+import StyledDiv from "./styled";
 
 const Profile = () => {
   const dispatch = useDispatch();
   const loged = useSelector((state) => state.loged);
-  const user = useSelector(state => state.user);
-  
+  const user = useSelector((state) => {
+    console.log(state);
+    return state.user;
+  });
   useEffect(() => {
     dispatch(getProfile());
   }, [dispatch]);
 
-  return <div>{user && loged === "on" ? 
-  <StyledDiv>
+  return (
     <div>
-      <h1>Bienvenido: {user[0].name} {user[0].lastName}</h1>
+      {loged === "on" ? (
+        user.map((el) => {
+          return (
+            <StyledDiv>
+              <div>
+                <h1>
+                  Bienvenido: {el.name} {el.lastName}
+                </h1>
+              </div>
+              <hr />
+              <div>
+                <h2>Email: {el.email}</h2>
+              </div>
+              <div>
+                <h4>Compras realizadas: </h4>
+                {el.orders.length > 0 ? (
+                  <div>{/* aca va el mapeo de los productos */}</div>
+                ) : (
+                  <div>
+                    <p>No posee compras realizadas</p>
+                  </div>
+                )}
+              </div>
+            </StyledDiv>
+          );
+        })
+      ) : (
+        <h1>No logueado</h1>
+      )}
     </div>
-    <hr/>
-    <div>
-      <h2>Email: {user[0].email}</h2>
-    </div>
-    <div>
-      <h4>Compras realizadas: </h4>
-      {
-        user[0].orders.length > 0 
-        ? <div>
-          {/* aca va el mapeo de los productos */}
-        </div>
-        : <div><p>No posee compras realizadas</p></div>
-      }
-    </div>
-  </StyledDiv> 
-  : <h1>No logueado</h1>}</div>;
+  );
 };
 
 export default Profile;
--- client/src/redux/actions/request.js
+++ client/src/redux/actions/request.js
@@ -148,7 +148,7 @@ export const getProfile = () => {
       });
     } catch (err) {
       apiRes = err.response.data.message;
-      dispatch({ type: LOAD_PROFILE, log: "off" });
+      dispatch({ type: LOAD_PROFILE, payload: { user: [], log: "off" } });
     }
   };
 };
