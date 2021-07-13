f71ad8f98a2b18d48c3c81a66e77bdaf48c14f65
Eduardo Salinas <eduardo.salinas1@outlook.com>
2021-07-13 17:04:45 -0300

    styles detail

--- client/src/components/ControlPanel/Users/UserDetail.jsx
+++ client/src/components/ControlPanel/Users/UserDetail.jsx
@@ -4,14 +4,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { editUserStatus, getUserDetail } from "../../../redux/actions";
 import { getUsers } from "../../../redux/actions";
 import swal from 'sweetalert';
+import { StyledUserDetail } from "../styled";
 
 const ItemUsers = ({ match }) => {
     // const fixed = useRef(match.params.id);
     const dispatch = useDispatch();
-    const user= useSelector (state=>state.userDetail)
-    useEffect(()=>{
+    const user = useSelector(state => state.userDetail)
+    useEffect(() => {
         dispatch(getUserDetail(match.params.id));
-    },[dispatch])
+    }, [dispatch])
 
     const handleClick = () => {
         dispatch(editUserStatus(user.email));
@@ -21,21 +22,22 @@ const ItemUsers = ({ match }) => {
 
     return (
         <StyledUserDetail>
-            <div>
-            <li className="name">Nombre: {user.name} {user.lastName}</li>
-            <li className="email">Email: {user.email}</li>
-            {user.address && <li className="adress">Direccion: {user.address}</li>}
-            <br />
-            
-            {user.orders && (user.orders.length >0 &&
-                <>
-                    <li> Ordenes:
-                        {user.orders.map(u => <li>{u}</li>)}
-                    </li>
-                </>)}
-            <li className="status">{user.userStatus}</li><Button variant="contained" onClick={handleClick}>Hacer Admin</Button>
-            <Button variant="contained" color="secondary" >Eliminar Usuario</Button>
-            <Button variant="contained" color="secondary" >Forzar reinicio de contraseña</Button>
+            <div >
+                <li className="name">Nombre: {user.name} {user.lastName}</li>
+                <li className="email">Email: {user.email}</li>
+                {user.address && <li className="adress">Direccion: {user.address}</li>}
+                <br />
+
+                {user.orders && (user.orders.length > 0 &&
+                    <>
+                        <li> Ordenes:
+                            {user.orders.map(u => <li>{u}</li>)}
+                        </li>
+                    </>)}
+                <li className="status">{user.userStatus}</li>
+                <Button className="btn" variant="contained" onClick={handleClick}>Hacer Admin</Button>
+                <Button className="btn" variant="contained" color="secondary" >Eliminar Usuario</Button>
+                <Button className="btn" variant="contained"  >Forzar reinicio de contraseña</Button>
             </div>
         </StyledUserDetail>
     )
