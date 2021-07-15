89fd04e943f1a336043e65fba1b1ebcc6dd5c95e
Emiliano Meneses <65609474+itsnou@users.noreply.github.com>
2021-07-15 11:59:13 -0300

    Checkout done (? Or await review

--- client/src/components/Checkout/Checkout.jsx
+++ client/src/components/Checkout/Checkout.jsx
@@ -1,14 +1,14 @@
 import {useEffect} from 'react';
 import {getProfile} from '../../redux/actions/request';
 import {useDispatch, useSelector} from 'react-redux';
-import { useForm } from "react-hook-form";
+import {Redirect, Link} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import StyledDiv from './styled';
 
 const Checkout = () => {
     const dispatch= useDispatch();
-    const loged = useSelector(state=> state.loged);
     const user = useSelector(state => state.user);
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
 
     useEffect(()=>{
         return dispatch(getProfile())
@@ -16,15 +16,27 @@ const Checkout = () => {
     
 
     return (
-        <>
-            <h1>Confirma tus datos para poder enviar los productos</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input defaultValue="test" {...register("example")} defaultValue={user[0].name}/>
-                <input {...register("exampleRequired", { required: true })} />
-                {errors.exampleRequired && <span>This field is required</span>}
-                <input type="submit" />
-            </form>
-        </>
+        <StyledDiv>
+            { window.sessionStorage.userLog === "on" ?
+                user.map((el)=>{
+                    return (
+                        <div className='container-form'>
+                            <h1>Hola! {el.name}</h1>
+                            <p>Elije el medio de pago que te parezca mejor.</p>
+                            <Grid container spacing={3}>
+                                <Grid item xs={6}>
+                                    <Button className='mercadoPago' variant="contained">Mercado Pago</Button>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Link to='/checkout/stripe' className='ref-link'>
+                                        <Button variant="contained">Tarjeta de Credito</Button>
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                    </div>)
+                }) : <Redirect to="/login" />
+            }
+        </StyledDiv>
     );
 }
  
