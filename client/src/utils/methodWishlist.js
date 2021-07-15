1ff5751d57c3f32862e5eb358d2ea925eb1d4c2c
manuelnsosa <manuelsosa84@gmail.com>
2021-07-15 17:25:00 -0300

    finished

--- client/src/utils/methodWishlist.js
+++
@@ -1,8 +0,0 @@
-export const validateWishlist = (product, wishlist) => {
-  let aux = false;
-  wishlist.length &&
-    wishlist.map((e) => {
-      if (e._id === product) aux = true;
-    });
-  return aux;
-};
