222b8342a478cc4db529476e8c7a9dbd8e95606b
Mr .reverse <75991527+julianmondaca@users.noreply.github.com>
2021-07-15 12:37:13 -0300

    mercadopago working

--- api/src/routes/mercadopago.js
+++ api/src/routes/mercadopago.js
@@ -2,39 +2,36 @@
 const {Router} = require('express');
 const router = Router();
 const mercadopago = require('mercadopago');
-const {MercadoPagoResponse} = require('mercadopago/utils/mercadopago-respose');
 
 mercadopago.configure({
 	access_token:
 		'TEST-3886982048825324-071417-78b25982e05ba3f21663b439b9a051e1-185367723',
 });
 
-router.post('/create_preference', (req, res) => {
+router.post('/create_preference', async (req, res) => {
 	const {name, quantity, price} = req.body;
+
 	const preference = {
 		items: [
 			{
-				name: name,
-				quantity: Number(quantity),
-				unit_price: Number(price),
+				title: name,
+				unit_price: price,
+				quantity: quantity,
 			},
 		],
 		back_urls: {
-			success: 'https://localhost:3000/success', //PONER PAGINA QUE QUEREMOS QUE NOS VAYA CUANDOS EFECTUA EL PAGO
+			success: 'https://localhost:3000/', //PONER PAGINA QUE QUEREMOS QUE NOS VAYA CUANDOS EFECTUA EL PAGO
 			pending: 'https://localhost:3000.com/pending',
 			failure: 'https://localhost:3000.com/error',
 		},
 		auto_return: 'approved',
 	};
-
-	mercadopago.preferences
-		.create(preference)
-		.then((res) => {
-			res.json({id: res.body.id});
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+	try {
+		const response = await mercadopago.preferences.create(preference);
+		res.send(response);
+	} catch (error) {
+		res.send(error);
+	}
 });
 
-mercadopago.preferences.create(preference);
+module.exports = router;
