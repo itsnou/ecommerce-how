13c982323a48a4a724e06cbc5cb0ff5fc8fe7744
Mariana Vinyolas <marianavinyolas@gmail.com>
2021-07-17 10:36:07 -0300

    Update styled.js
    Cambio el rojo en el hoover del nav

--- client/src/components/Nav/styled.js
+++ client/src/components/Nav/styled.js
@@ -46,7 +46,8 @@ const StyledDiv = styled.div`
                     text-decoration:none;
                     color:white;
                     :hover{
-                        color: red;
+                        /* color: red; */
+                        color: #FADDD3;
                     }
                 }
             }
