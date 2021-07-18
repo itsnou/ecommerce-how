d83754bbcde9b5b90227a71f4ed37d3ea0e1b2c0
Juan Sebastian Salazar <juansebastiansalazargonzalez@gmail.com>
2021-07-17 20:21:18 -0500

    commit
    Co-Authored-By: Eduardo Salinas <75259006+eduardo-salinas@users.noreply.github.com>
    Co-Authored-By: Emiliano Meneses <65609474+itsnou@users.noreply.github.com>
    Co-Authored-By: Manuel Sosa <71500229+manuelnsosa@users.noreply.github.com>
    Co-Authored-By: Tomas Buzzalino <tomibuzzalino@gmail.com>
    Co-Authored-By: Juan Condori <78325127+juancdri@users.noreply.github.com>

--- client/src/components/ControlPanel/Search.jsx
+++ client/src/components/ControlPanel/Search.jsx
@@ -1,68 +1,72 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { HiOutlineSearch } from 'react-icons/hi';
-import { getOrderForUser, getProductsByName, getUsers, reset, userFiltered } from '../../redux/actions';
+import React, { useState, useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { HiOutlineSearch } from "react-icons/hi";
+import {
+  getOrderForUser,
+  getProductsByName,
+  getUsers,
+  reset,
+  userFiltered,
+} from "../../redux/actions";
 
-import Button from '@material-ui/core/Button';
-import { StyledSearch } from './styled';
+import Button from "@material-ui/core/Button";
+import { StyledSearch } from "./styled";
 
 const Search = ({ itemValue }) => {
-    const [input, setInput] = useState('');
-    const entry = useRef(input.length)
-    const dispatch = useDispatch();
-    const store = useSelector(state => state);
+  const [input, setInput] = useState("");
+  const entry = useRef(input.length);
+  const dispatch = useDispatch();
+  const store = useSelector((state) => state);
 
-    const handleChange = (e) => {
-        setInput(e.target.value);
-    };
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        switch (itemValue) {
-            case 'product':
-                dispatch(getProductsByName(input));
-                break;
-            case 'user':
-                dispatch(getUsers());
-                dispatch(userFiltered(input, store.users));
-                break;
-            case 'order':
-                dispatch(getOrderForUser(input));
-                break;
-            default:
-                setInput('');
-        }
-        setInput('');
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    switch (itemValue) {
+      case "product":
+        dispatch(getProductsByName(input));
+        break;
+      case "user":
+        dispatch(getUsers());
+        dispatch(userFiltered(input, store.users));
+        break;
+      case "order":
+        dispatch(getOrderForUser(input));
+        break;
+      default:
+        setInput("");
+    }
+    setInput("");
+  };
 
-    useEffect(() => {
-        if (entry.current === 0) {
-            setInput('');
-        }
-        return dispatch(reset("searchUser"))
-    }, [dispatch]);
+  useEffect(() => {
+    if (entry.current === 0) {
+      setInput("");
+    }
+    return dispatch(reset("searchUser"));
+  }, [dispatch]);
 
-    return (
-        <StyledSearch>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type='search'
-                        placeholder='Buscar por nombre...'
-                        value={input}
-                        onChange={handleChange}
-                    />
-                    <Button
-                        className='btn'
-                        variant='contained'
-                        onClick={handleSubmit}
-                    >
-                        <HiOutlineSearch />
-                    </Button>
-                </form>
-            </div>
-        </StyledSearch>
-    );
+  return (
+    <StyledSearch>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Buscar por nombre..."
+            value={input}
+            onChange={handleChange}
+          />
+          <Button className="btn" variant="contained" onClick={handleSubmit}>
+            <HiOutlineSearch />
+          </Button>
+        </form>
+      </div>
+    </StyledSearch>
+  );
 };
 
 export default Search;
+
+//cualquier cambio
