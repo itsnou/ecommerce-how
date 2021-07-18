f56349a43750219850daf1b192e6a58ce534b1ae
Emiliano Meneses <65609474+itsnou@users.noreply.github.com>
2021-07-17 22:26:11 -0300

    Add Form product!
    Co-Authored-By: Juan Sebastian Salazar  <juansebastiansalazargonzalez@gmail.com>
    Co-Authored-By: Tomas Buzzalino <tomibuzzalino@gmail.com>
    Co-Authored-By: Manuel Sosa <71500229+manuelnsosa@users.noreply.github.com>
    Co-Authored-By: Eduardo Salinas <75259006+eduardo-salinas@users.noreply.github.com>

--- client/src/components/ControlPanel/AddProduct.jsx
+++ client/src/components/ControlPanel/AddProduct.jsx
@@ -4,6 +4,9 @@ import { addProduct } from '../../redux/actions/sending';
 import { getVarietals } from '../../redux/actions/request';
 import { Link } from 'react-router-dom';
 import { useForm } from "react-hook-form";
+import {StyledAddProduct} from './styled';
+import Button from '@material-ui/core/Button';
+
 
 const AddProduct = () => {
     const { register, handleSubmit, formState:{ errors } } = useForm();
@@ -34,132 +37,145 @@ const AddProduct = () => {
     }, [dispatch, varietals]);
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input placeholder="Nombre"
-                {...register("name", {
-                    required: {
-                        value: true,
-                        message: 'El nombre es requerido'
-                    }
-                })
-                } />
-           <p>{errors.name?.message}</p>
-            <input placeholder="Bodega"
-                {...register("vineyard", {
-                    required: {
-                        value: true,
-                        message: 'El nombre de la bodega es requerido'
-                    }
-                })
-                } />
-            <span>{errors.vineyard?.message}</span>
-            <input placeholder="Url de Imagen"
-                {...register("imageUrl", {
-                    required: {
-                        value: true,
-                        message: 'El URL de la imagen es requerido'
-                    }
-                })
-                } />
-            <span>{errors.imageUrl?.message}</span>
-            <label>Categoría</label>
-            <select {...register("category", {
-                required: {
-                    value: true,
-                    message: 'Seleccione una opción'
-                }
-            })}
-            onChange={e=>handleChange(e)}
-            >
-                <option  value="vacio">Elegir una categoría</option>
-                <option value="Tinto">Tinto</option>
-                <option value="Rosado">Rosado</option>
-                <option value="Blanco">Blanco</option>
-            </select>
-            <span>{errors.category?.message}</span>
-            {/* <label>Varietal</label> */}
-            <select {...register("varietal", {
-                required: {
-                    value: true,
-                    message: 'Seleccione una o más opciones'
-                }
-            })} multiple>
-                {uvas.map((e,i) => (<option key={i} value={e}>{e}</option>))}
-            </select>
-            <span>{errors.varietal?.message}</span>
-            <input placeholder="Precio" type="number"
-                {...register("price", {
-                    required: {
-                        valueAsNumber: true,
-                        message: 'El precio es requerido'
-                    },
-                    min: {
-                        value: 0,
-                        message: 'El precio es requerido'
-                    }
-                })
-                } />
-            <span>{errors.price?.message}</span>
-            
-            <input placeholder="Stock" type="number"
-                {...register("stock", {
-                    required: {
-                        valueAsNumber: true,
-                        message: 'El stock es requerido'
-                    },
-                    min: {
-                        value: 0,
-                        message: 'El stock no puede ser negativo'
-                    }
-                })
-                } />
-            <span>{errors?.stock?.message}</span>
-            
-            <input placeholder="Año de cosecha" type="number"
-                {...register("year", {
-                    required: {
-                        valueAsNumber: true,
-                        message: 'El año es requerido'
-                    },
-                    max: {
-                        value: 2021,
-                        message: 'El año no es valido'
-                    },
-                    min: {
-                        value: 2010,
-                        message: 'El año no es valido'
-                    }
-                })
-                } />
-            <span>{errors?.year?.message}</span>
-            <input placeholder="Descripción"
-                {...register("description", {
-                    required: {
-                        value: true,
-                        message: 'La descripción es requerida'
-                    }
-                })
-                } />
-            <span>{errors?.description?.message}</span>
+        <StyledAddProduct>
+            <form className='add-product' onSubmit={handleSubmit(onSubmit)}>
+                <label>
+                    <h4>Nombre: </h4>
+                    <input placeholder="Nombre"
+                        {...register("name", {
+                            required: {
+                                value: true,
+                                message: 'El nombre es requerido'
+                            }
+                        })
+                        } />
+                    <p>{errors.name?.message}</p>
+                </label>
+                <label>
+                    <h4>Nombre de la bodega: </h4>
+                    <input placeholder="Bodega"
+                        {...register("vineyard", {
+                            required: {
+                                value: true,
+                                message: 'El nombre de la bodega es requerido'
+                            }
+                        })
+                        } />
+                    <span>{errors.vineyard?.message}</span>
+                </label>
+                <label>
+                    <h4>Imagen: </h4>
+                    <input placeholder="Url de Imagen"
+                        {...register("imageUrl", {
+                            required: {
+                                value: true,
+                                message: 'El URL de la imagen es requerido'
+                            }
+                        })
+                        } />
+                    <span>{errors.imageUrl?.message}</span>
+                </label>
+                <label>
+                    <h4>Categoría</h4>
+                    <select {...register("category", {
+                        required: {
+                            value: true,
+                            message: 'Seleccione una opción'
+                        }
+                    })}
+                    onChange={e=>handleChange(e)}
+                    >
+                        <option  value="vacio">Elegir una categoría</option>
+                        <option value="Tinto">Tinto</option>
+                        <option value="Rosado">Rosado</option>
+                        <option value="Blanco">Blanco</option>
+                    </select>
+                    <span>{errors.category?.message}</span>
+                </label>
+                <label>
+                    <h4>Varietales: </h4>
+                    <select {...register("varietal", {
+                        required: {
+                            value: true,
+                            message: 'Seleccione una o más opciones'
+                        }
+                    })} multiple>
+                        {uvas.map((e,i) => (<option key={i} value={e}>{e}</option>))}
+                    </select>
+                    <span>{errors.varietal?.message}</span>
+                </label>
+                <label>
+                    <h4>Precio: </h4>
+                    <input placeholder="Precio" type="number"
+                        {...register("price", {
+                            required: {
+                                valueAsNumber: true,
+                                message: 'El precio es requerido'
+                            },
+                            min: {
+                                value: 0,
+                                message: 'El precio es requerido'
+                            }
+                        })
+                        } />
+                    <span>{errors.price?.message}</span>
+                </label>
+                <label>
+                    <h4>Stock disponible: </h4>
+                    <input placeholder="Stock" type="number"
+                        {...register("stock", {
+                            required: {
+                                valueAsNumber: true,
+                                message: 'El stock es requerido'
+                            },
+                            min: {
+                                value: 0,
+                                message: 'El stock no puede ser negativo'
+                            }
+                        })
+                        } />
+                    <span>{errors?.stock?.message}</span>
+                </label>
+                <label>
+                    <h4>Año de cosecha: </h4>
+                    <input placeholder="Año de cosecha" type="number"
+                        {...register("year", {
+                            required: {
+                                valueAsNumber: true,
+                                message: 'El año es requerido'
+                            },
+                            max: {
+                                value: 2021,
+                                message: 'El año no es valido'
+                            },
+                            min: {
+                                value: 2010,
+                                message: 'El año no es valido'
+                            }
+                        })
+                        } />
+                    <span>{errors?.year?.message}</span>
+                </label>
+                <label>
+                    <h4>Descripcion: </h4>
+                    <textarea placeholder="Descripción"
+                        {...register("description", {
+                            required: {
+                                value: true,
+                                message: 'La descripción es requerida'
+                            }
+                        })
+                        } rows={5}/>
+                    <span>{errors?.description?.message}</span>
+                </label>
 
-
-
-
-            {/* {error === -1 ? <button type="submit" className="btn-submit">Agregar Producto</button> : <span className={error && 'danger'} >{error}</span>} */}
-            {/* <input type="submit" value=" Create Recipe" onClick={(e) => handleSubmit(e)}/> */}
-            {/* <button type="submit" > Create Recipe </button> */}
-            <button type="submit" >Agregar Producto</button>
-            <Link to={`/catalogo`}><button >Volver</button></Link>
-
-        </form>
+                {/* {error === -1 ? <button type="submit" className="btn-submit">Agregar Producto</button> : <span className={error && 'danger'} >{error}</span>} */}
+                {/* <input type="submit" value=" Create Recipe" onClick={(e) => handleSubmit(e)}/> */}
+                <Button className='block' type="submit" >Agregar Producto</Button>
+                <Link to={`/catalogo`}><Button className='volver'>Volver</Button></Link>
+            </form>
+        </StyledAddProduct>
     )
 };
 
 export default AddProduct;
-// onChange={(e) => validater(e.target.value, 'name',)}
-// onChange={(e) => validater(e.target.value, 'vineyard')}
-// onChange={(e) => validater(e.target.value, 'imageUrl')}
-// onChange={(e) => validater(e.target.value, 'varietal')} 
-// onChange={(e) => validater(e.target.value, 'category')} 
-// onChange={(e) => validater(e.target.value, 'stock')}
-// onChange={(e) => validater(e.target.value, 'discount')}
