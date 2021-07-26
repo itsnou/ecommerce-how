4edebef8258ffe4570c6af508a1364a6ff643c09
Mariana Vinyolas <marianavinyolas@gmail.com>
2021-07-26 14:14:48 -0300

    Update ItemVineyard.jsx
    Co-Authored-By: Juan Condori <78325127+juancdri@users.noreply.github.com>
    Co-Authored-By: Mr .reverse <75991527+julianmondaca@users.noreply.github.com>
    Co-Authored-By: Eduardo Salinas <75259006+eduardo-salinas@users.noreply.github.com>
    Co-Authored-By: Tomas Buzzalino <tomibuzzalino@gmail.com>

--- client/src/components/ControlPanel/Vineyards/ItemVineyard.jsx
+++ client/src/components/ControlPanel/Vineyards/ItemVineyard.jsx
@@ -3,6 +3,8 @@ import { StyledProduct } from "../styled.js";
 import Button from "@material-ui/core/Button";
 import { useDispatch } from "react-redux";
 import { editProductsByVineyard } from '../../../redux/actions/sending'
+import swal from "sweetalert";
+
 
 const ItemVineyard = (props) => {
   const dispatch = useDispatch();
@@ -26,6 +28,39 @@ const ItemVineyard = (props) => {
     })
   }
 
+  const handleClick = () => {
+    if (input.increase === 1 && input.discount === 1) {
+      swal({ title: "Ingresar un valor", icon: "warning" });
+    } else {
+      swal({
+        title: "¿Estás seguro?",
+        text: "Estás por modificar el precio de este producto",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+            dispatch(editProductsByVineyard(input))
+            swal("Precio modificado", {
+              icon: "success",
+            });
+          } else {
+            swal("Acción cancelada", {
+              icon: "error",
+            });
+          }
+        });
+      }
+      setInput({
+        vineyard: props.name,
+        increase: 1,
+        discount: 1,
+      })
+      document.getElementById('increase').value = ''
+      document.getElementById('discount').value = ''
+  }
+
   return (
     <StyledProduct>
           <li className="name">{props.name}</li>
@@ -40,6 +75,7 @@ const ItemVineyard = (props) => {
             name='increase'
             placeholder= 'Ingrese un %'
             onChange={(e)=>handleChange(e)}
+            id='increase'
             >
             </input>
 
@@ -54,11 +90,12 @@ const ItemVineyard = (props) => {
             name='discount'
             placeholder='Ingrese un %'
             onChange={(e)=>handleChange(e)}
+            id='discount'
             ></input>
           </div>
           <Button 
           variant="contained"
-          onClick={()=>dispatch(editProductsByVineyard(input))}
+          onClick={()=>handleClick()}
           >EDITAR</Button>
     </StyledProduct>
   )
