bc4f4e235fbb34cfc69994d9a273cf64ae33cee5
Mariana Vinyolas <marianavinyolas@gmail.com>
2021-07-26 15:31:57 -0300

    Checkout styled
    Cambios en el detalle de compra antes de pagar
    
    Co-Authored-By: Juan Condori <78325127+juancdri@users.noreply.github.com>
    Co-Authored-By: Mr .reverse <75991527+julianmondaca@users.noreply.github.com>
    Co-Authored-By: Eduardo Salinas <75259006+eduardo-salinas@users.noreply.github.com>
    Co-Authored-By: Tomas Buzzalino <tomibuzzalino@gmail.com>

--- client/src/components/Checkout/StripePayment/styled.js
+++ client/src/components/Checkout/StripePayment/styled.js
@@ -1,23 +1,54 @@
 import styled from 'styled-components';
 
 const StyledDiv = styled.div`
-    max-width:450px;
+    width:50%;
     margin:15px auto;
     border: 1px solid black;
     border-radius:7px;
+    background-color: #420D1F;
+    color: white;
+    h1 {
+        margin-left: 50px;
+    }
+    h2 {
+        margin-left: 50px;
+    }
+    .detail {
+        li {
+            display: grid;
+            grid-template-columns: 3fr 1fr 1fr;
+            list-style: none;
+            font-size: 18px;
+            background-color: white;
+            color: black;
+            font-weight: bold;
+            margin: 10px;
+            border-radius: 4px;
+            width: 90%;
+            padding: 10px;
+        }
+    }
     .form-container{
-        padding:25px;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        padding:25px 50px 50px 50px;
+        width: 90%;
         .form-control{
             background-color: #F0F0F0;
             padding:10px;
             border-radius:3px;
         }
         button{
-            margin-top:10px;
-            background-color:#420D1F;
             color:white;
             font-weight:bold;
-            width:100%;
+            margin: 0px 20px;
+            background-color:#555555;
+            color:white;
+            :hover{
+                background-color:white;
+                color:black;
+                transition: 200ms;
+            }
         }
     }
 `
