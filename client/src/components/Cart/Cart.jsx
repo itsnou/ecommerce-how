d29587bd91d5bc6a77886710457a06949f67ce21
Mr .reverse <75991527+julianmondaca@users.noreply.github.com>
2021-07-27 13:23:54 -0300

    CART UPDATED
    Co-Authored-By: Tomas Buzzalino <tomibuzzalino@gmail.com>

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,118 +1,124 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { removeFromCart, modifyItemCart } from "../../redux/actions/cart";
+import {useSelector, useDispatch} from "react-redux";
+import {removeFromCart, modifyItemCart} from "../../redux/actions/cart";
 import StyledCartItems from "./styled.js";
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import {Link} from 'react-router-dom';
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import {Link} from "react-router-dom";
 
 export const Cart = () => {
-  const dispatch = useDispatch();
-  const cartItems = useSelector((state) => state.cart);
-  let total = 0;
-  //funcion onclick pa' despachar la action
+	const dispatch = useDispatch();
+	const cartItems = useSelector((state) => state.cart);
+	let total = 0;
+	//funcion onclick pa' despachar la action
 
+	const handleOnClick = (e, product) => {
+		if (e === "x") {
+			dispatch(removeFromCart(product._id));
+			localStorage.removeItem(`${product.name}`);
+		} else {
+			let newQuantity = product.quantity;
+			if (e === "+" && newQuantity < product.stock) {
+				newQuantity++;
+			} else {
+				if (newQuantity === 1) return;
+				newQuantity--;
+			}
+			let obj = {
+				...product,
+				quantity: newQuantity,
+			};
+			dispatch(modifyItemCart(obj));
+			window.localStorage.setItem(`${product.name}`, JSON.stringify(obj));
+		}
+	};
 
-  const handleOnClick = (e, product) => {
-    if (e === "x") {
-      dispatch(removeFromCart(product._id));
-      localStorage.removeItem(`${product.name}`)
-    } else {
-      let newQuantity = product.quantity;
-      if (e === "+" && newQuantity < product.stock) {
-        newQuantity++;
-      } else {
-        if (newQuantity === 1) return;
-        newQuantity--;
-      }
-      let obj = {
-        ...product,
-        quantity: newQuantity,
-      };
-      dispatch(modifyItemCart(obj));
-      window.localStorage.setItem(`${product.name}`, JSON.stringify(obj));
-    }
-  };
-
-  return (
-    <>
-      <StyledCartItems>
-        {cartItems.length ?
-          cartItems.map((e, index) => {
-            total = total + e.price * e.quantity;
-            return (
-              <div key={index} className='container-cards_products'>
-                <div className='container-img_card'>
-                  <img className="img-card" src={e.imageUrl} alt={e.name}/>
-                </div>
-                <div className='container-title'>
-                  <h2>{e.name}</h2>
-                </div>
-                <div className="container-btn">
-                  <button
-                    className="btn-item-cart"
-                    onClick={() => {
-                      handleOnClick("-", e);
-                    }}
-                  >
-                    -
-                  </button>
-                  <h2>{e.quantity}</h2>
-                  <button
-                    className="btn-item-cart"
-                    name="+"
-                    onClick={() => {
-                      handleOnClick("+", e);
-                    }}
-                  >
-                    +
-                  </button>
-                </div>
-                <div className='product-card_price'>
-                  <h2>Stock: <span>{e.stock}</span></h2>
-                  <h2>$ {e.price * e.quantity}</h2>
-                </div>
-                <div className='btn-delete'>
-                  <button
-                    className="btn-item-delete"
-                    onClick={() => {
-                      handleOnClick("x", e);
-                    }}
-                  >
-                    X
-                  </button>
-                </div>
-              </div>
-            );
-          }):
-            <h1>No hay ningún producto en el carrito</h1>
-          }
-          <hr/>
-        <div className='cart-total'>
-          <div className="total">
-            <Grid
-              container
-              direction="row"
-              justifyContent="space-between"
-              alignItems="center"
-            >
-              <Grid item xs={6}>
-                <h2>Total: $ {total}</h2>
-              </Grid>
-              <Grid item xs={6}>
-              { window.sessionStorage.userLog === 'on'  && cartItems.length> 0 ? 
-                <Link to='/checkout'>
-                 <Button variant="contained">COMPRAR!</Button>
-                </Link>
-                : null
-              }
-              </Grid>
-            </Grid>
-          </div>
-        </div>
-      </StyledCartItems>
-    </>
-  );
+	return (
+		<>
+			<StyledCartItems>
+				<div className="cart-total">
+					<div className="total">
+						<Grid
+							container
+							direction="row"
+							justifyContent="space-between"
+							alignItems="center"
+						>
+							<Grid item xs={6}>
+								<h2>Total: $ {total}</h2>
+							</Grid>
+							<Grid item xs={6}>
+								{window.sessionStorage.userLog === "on" &&
+								cartItems.length > 0 ? (
+									<Link to="/checkout">
+										<Button variant="contained">COMPRAR!</Button>
+									</Link>
+								) : (
+									<Link to="/create">
+										<Button variant="contained">REGISTRARSE</Button>
+									</Link>
+								)}
+							</Grid>
+						</Grid>
+					</div>
+				</div>
+				<hr />
+				{cartItems.length ? (
+					cartItems.map((e, index) => {
+						total = total + e.price * e.quantity;
+						return (
+							<div key={index} className="container-cards_products">
+								<div className="container-img_card">
+									<img className="img-card" src={e.imageUrl} alt={e.name} />
+								</div>
+								<div className="container-title">
+									<h2>{e.name}</h2>
+								</div>
+								<div className="container-btn">
+									<button
+										className="btn-item-cart"
+										onClick={() => {
+											handleOnClick("-", e);
+										}}
+									>
+										-
+									</button>
+									<h2>{e.quantity}</h2>
+									<button
+										className="btn-item-cart"
+										name="+"
+										onClick={() => {
+											handleOnClick("+", e);
+										}}
+									>
+										+
+									</button>
+								</div>
+								<div className="product-card_price">
+									<h2>
+										Stock: <span>{e.stock}</span>
+									</h2>
+									<h2>$ {e.price * e.quantity}</h2>
+								</div>
+								<div className="btn-delete">
+									<button
+										className="btn-item-delete"
+										onClick={() => {
+											handleOnClick("x", e);
+										}}
+									>
+										X
+									</button>
+								</div>
+							</div>
+						);
+					})
+				) : (
+					<h1 className="h1Cart">No hay ningún producto en el carrito</h1>
+				)}
+			</StyledCartItems>
+		</>
+	);
 };
 
 export default Cart;
--- client/src/components/Cart/styled.js
+++ client/src/components/Cart/styled.js
@@ -1,125 +1,129 @@
 import styled from "styled-components";
 
 const StyledCartItems = styled.div`
-  color:white;
-  width: 95%;
-  border-radius:8px;
-  margin:auto;
-  margin-top:20px;
-  hr{
-    width:90%;
-  }
+	color: white;
+	width: 95%;
+	border-radius: 8px;
+	margin: auto;
+	margin-top: 20px;
+	hr {
+		width: 90%;
+	}
 
-  .container-cards_products{
-    background-color: #420D1F;
-    border:1px solid black;
-    padding: 1em 0;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-    align-items: center;
-    justify-content: space-between;
-    width:95%;
-    margin:auto;
-    margin-bottom:15px;
-    border-radius:10px;
-    .container-title{
-      width:15em;
-    }
+	.h1Cart {
+		color: black;
+		text-align: center;
+	}
 
-    .container-btn {
-      width:75%;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      text-align: center;
-      background-color:white;
-      color:black;
-      border-radius:7px;
-      h2{
-        margin:30px;
-        span{
-          display:flex;
-          flex-direction:column;
-        }
-      }
-    }
+	.container-cards_products {
+		background-color: #420d1f;
+		border: 1px solid black;
+		padding: 1em 0;
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+		align-items: center;
+		justify-content: space-between;
+		width: 95%;
+		margin: auto;
+		margin-bottom: 15px;
+		border-radius: 10px;
+		.container-title {
+			width: 15em;
+		}
 
-    .product-card_price{
-      text-align:center;
-    }
-    
-    .btn-item-cart {
-      background-color: #420D1F;
-      margin: 10px;
-      border-radius: 3px;
-      padding: 4px;
-      color: white;
-      border: none;
-      font-weight: bold;
-      width: 2em;
-      height: 2em;
-      font-weight: bold;
+		.container-btn {
+			width: 75%;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			text-align: center;
+			background-color: white;
+			color: black;
+			border-radius: 7px;
+			h2 {
+				margin: 30px;
+				span {
+					display: flex;
+					flex-direction: column;
+				}
+			}
+		}
 
-      :hover {
-        background-color: black;
-        color: white;
-        cursor: pointer;
-      }
-    }
-    
-    .btn-delete{
-      margin:auto;
-      text-align:center;
-      button{
-        color:black;
-        background-color: white;
-        border:none;
-        padding:4px;
-        border-radius:3px;
-        width: 2em;
-        height: 2em;
-        font-weight:bold;
-        cursor:pointer
-      }
-    }
+		.product-card_price {
+			text-align: center;
+		}
 
-    .container-img_card{
-      text-align: center;
-      width:150px;
-      background-color:white;
-      border-radius: 7px;
-      margin:auto;
-      .img-card {
-        height: 9em;
-        width: 2em;
-        object-fit: cover;
-        object-position: center center;
-      }
-    }
+		.btn-item-cart {
+			background-color: #420d1f;
+			margin: 10px;
+			border-radius: 3px;
+			padding: 4px;
+			color: white;
+			border: none;
+			font-weight: bold;
+			width: 2em;
+			height: 2em;
+			font-weight: bold;
 
-  }
-    .cart-total{
-      padding-bottom:10px;
-      height:40px;
-      margin-bottom:10px;
-      background-color: #420D1F;
-      border-radius:7px;
-      .total {
-        width:100%;
-        text-align:center;
-        padding:0;
-        h2{
-          padding:0;
-          margin:0;
-        }
-        button{
-          margin-top:4px;
-          background-color:white;
-          color:black;
-          font-weight:bold;
-        }
-      }
-    }
+			:hover {
+				background-color: black;
+				color: white;
+				cursor: pointer;
+			}
+		}
+
+		.btn-delete {
+			margin: auto;
+			text-align: center;
+			button {
+				color: black;
+				background-color: white;
+				border: none;
+				padding: 4px;
+				border-radius: 3px;
+				width: 2em;
+				height: 2em;
+				font-weight: bold;
+				cursor: pointer;
+			}
+		}
+
+		.container-img_card {
+			text-align: center;
+			width: 150px;
+			background-color: white;
+			border-radius: 7px;
+			margin: auto;
+			.img-card {
+				height: 9em;
+				width: 2em;
+				object-fit: cover;
+				object-position: center center;
+			}
+		}
+	}
+	.cart-total {
+		padding-bottom: 10px;
+		height: 40px;
+		margin-bottom: 10px;
+		background-color: #420d1f;
+		border-radius: 7px;
+		.total {
+			width: 100%;
+			text-align: center;
+			padding: 0;
+			h2 {
+				padding: 0;
+				margin: 0;
+			}
+			button {
+				margin-top: 4px;
+				background-color: white;
+				color: black;
+				font-weight: bold;
+			}
+		}
+	}
 `;
 
 export default StyledCartItems;
